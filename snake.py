7d8ceee1ca58178294cbfa78959753f0f1ca7fa8
nikita <Nikita@gmal.com>
2022-01-20 22:40:53 +0200

    Added file

--- snake.py
+++ snake.py
@@ -1,6 +1,27 @@
 def move():
     pass
 def draw():
-    pass
+    snake = [{"snakeX": 2, "snakeY": 1},
+             {"snakeX": 3, "snakeY": 1},
+             {"snakeX": 4, "snakeY": 1},
+             {"snakeX": 4, "snakeY": 2},
+             {"snakeX": 4, "snakeY": 3},
+             {"snakeX": 4, "snakeY": 4}
+             ]
+
+    item = snake[0]
+    y = 0
+    while y < 20:
+        result = ""
+        x = 0
+        while x < 100:
+            char = 'o'
+            for item in snake:
+                if item['snakeX'] == x and item['snakeY'] == y:
+                    char = "g"
+            result += char
+            x += 1
+        print(result)
+        y += 1
  while True:
      pass
