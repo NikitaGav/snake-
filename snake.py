70ddccb562b28761d6b3d7c71c28da779b16cff4
pikadvert <pik.advert@gmail.com>
2022-01-20 22:04:10 +0200

    edit moveSnake

--- snake.py
+++ snake.py
@@ -2,13 +2,17 @@ snake = [{'x': 2, 'y': 1},
          {'x': 3, 'y': 1},
          {'x': 4, 'y': 1}
          ]
-direction = 1
+direction = 2
 #1-up
 #2-down
 def move():
     global direction
-    new_head = snake[-1]
-    snake.append({'x': 5, 'y': 1})
+    new_head = snake[-1].copy()
+    if direction == 1:
+      new_head['y'] -= 1
+    if direction == 2:
+      new_head['y'] += 1
+    snake.append(new_head)
     snake.pop(0)
     print(snake)
 
