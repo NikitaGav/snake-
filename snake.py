759b52f83880d230bd9042a0d03a1094f61f00dd
pikadvert <pik.advert@gmail.com>
2022-01-20 21:47:45 +0200

    Added moveSnake

--- snake.py
+++ snake.py
@@ -1,5 +1,20 @@
+snake = [{'x': 2, 'y': 1},
+         {'x': 3, 'y': 1},
+         {'x': 4, 'y': 1}
+         ]
+direction = 1
+#1-up
+#2-down
 def move():
-    pass
+    global direction
+    new_head = snake[-1]
+    snake.append({'x': 5, 'y': 1})
+    snake.pop(0)
+    print(snake)
+
+move()
+
+
 def draw():
     pass
  while True:
