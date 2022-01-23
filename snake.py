58b1b4e68829c4faa90a037c8e6da0ff9f4146ce
NikitaGav <97907205+NikitaGav@users.noreply.github.com>
2022-01-23 16:44:47 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,3 +1,7 @@
+import pynput
+import os
+import time
+
 def move():
     pass
 def draw():
@@ -23,5 +27,34 @@ def draw():
             x += 1
         print(result)
         y += 1
- while True:
-     pass
+
+direction = 2
+
+
+def press(key):
+    global direction
+    if key == pynput.keyboard.Key.down:
+        direction = 2
+    if key == pynput.keyboard.Key.up:
+        direction = 1
+    if key == pynput.keyboard.Key.left:
+        direction = 4
+    if key == pynput.keyboard.Key.right:
+        direction = 3
+
+def on_release(key):
+    pass
+
+pynput.keyboard.Listener(
+    on_press = on_press,
+    on_release = on_release
+    ).start()
+    
+
+def draw():
+    pass
+while True:
+    os.system('cls')
+    move()
+    draw()
+    time.sleep(0.3)
