c7feb9604d7eab083844c1713db60712e91051cb
pikadvert <pik.advert@gmail.com>
2022-01-23 14:36:26 +0200

    moveSnake_ready

--- snake.py
+++ snake.py
@@ -5,18 +5,34 @@ snake = [{'x': 2, 'y': 1},
 direction = 2
 #1-up
 #2-down
+#3- right
+#4-left
 def move():
-    global direction
+    global direction, food
     new_head = snake[-1].copy()
     if direction == 1:
       new_head['y'] -= 1
     if direction == 2:
       new_head['y'] += 1
+    if direction == 3:
+      new_head['x'] += 1
+    if direction == 4:
+      new_head['x'] -= 1
+    if new_head['x'] < 0 or new_head['x'] > 29 or new_head['y'] < 0 or new_head['y'] > 14:
+        exit("Game over")
+    for item in snake:
+        if new_head['x'] == item['x'] and new_head['y'] == item['y']:
+            exit("Game over")
+
+    if new_head['x'] == food['x'] and new_head['y'] == food['y']:
+        generate_food()
+    else:
+        snake.pop(0)
     snake.append(new_head)
-    snake.pop(0)
-    print(snake)
+    
 
 move()
+
 
 
 def draw():
